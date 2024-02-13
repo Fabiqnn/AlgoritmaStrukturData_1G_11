5bf7e256930a8064eaaa00f58460007ca9f2430c
Fabiqnn <arema20056@gmail.com>
2024-02-13 12:13:12 +0700

    Perulangan1_11

---
+++ Perulangan1_11.java
@@ -0,0 +1,41 @@
+/**
+ * Perulangan1_11
+ */
+import java.util.*;
+public class Perulangan1_11 {
+
+    public static void main(String[] args) {
+        Scanner sc11 = new Scanner(System.in);
+
+        double Nim;
+        System.out.print("Masukkan NIM Anda: ");
+
+        Nim = sc11.nextDouble();
+        System.out.println("=====================");
+        int DuaDigit = (int) (Nim % 100);
+        if (DuaDigit < 10) {
+            DuaDigit += 10;
+            System.out.println("N = " + DuaDigit);
+            for (int i = 1 ; i <= DuaDigit; i++) {
+                if (i % 2 == 1) {
+                    System.out.print(" *");
+                } else if ((i == 6) || (i == 10)) {
+                    continue;
+                } else {
+                    System.out.print(" "+ i);
+                }
+            }
+        } else {
+            System.out.println("N = " + DuaDigit);
+            for (int i = 1 ; i <= DuaDigit; i++) {
+                if (i % 2 == 1) {
+                    System.out.print(" *");
+                } else if ((i == 6) || (i == 10)) {
+                    continue;
+                } else {
+                    System.out.print(" "+ i);
+                }
+            }
+        }
+    }
+}
