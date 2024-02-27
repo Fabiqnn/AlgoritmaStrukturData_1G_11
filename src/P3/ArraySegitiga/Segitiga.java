30a1803cd46e1b6022fb592e12122173fc056e77
Fabiqnn <arema20056@gmail.com>
2024-02-28 01:39:22 +0700

    P3_Percobaan 3_Pertanyaan 3_11

--- src/P3/ArraySegitiga/Segitiga.java
+++ src/P3/ArraySegitiga/Segitiga.java
@@ -8,4 +8,18 @@ public class Segitiga {
         alas = a;
         tinggi = t;
     }
+
+    public int hitungLuas () {
+        return alas * tinggi;
+    }
+
+    public int hitungKeliling () {
+        return alas + tinggi + sisiMiring();
+    }
+
+    public int sisiMiring() {
+        double sisiMiring = Math.sqrt(Math.pow(alas, 2) + Math.pow(tinggi, 2));
+        int intSisiMiring = (int) sisiMiring;
+        return intSisiMiring;
+    }
 }
