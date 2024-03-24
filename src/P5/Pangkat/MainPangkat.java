f86d96e737771501d1a4d7fa1630253100060280
Fabiqnn <arema20056@gmail.com>
2024-03-24 18:41:35 +0700

    P5_Percobaan 2_11

---
+++ src/P5/Pangkat/MainPangkat.java
@@ -0,0 +1,38 @@
+package P5.Pangkat;
+
+import java.util.Scanner;
+
+/**
+ * MainPangkat
+ */
+public class MainPangkat {
+
+    public static void main(String[] args) {
+        Scanner sc11 = new Scanner(System.in);
+        System.out.println("-------------------------");
+        System.out.print("Masukkan Jumlah Elemen yang Dihitung : ");
+        int elemen = sc11.nextInt();
+
+        Pangkat[] pangkatArr = new Pangkat[elemen];
+        for (int i = 0; i < pangkatArr.length; i++) {
+            System.out.print("Masukkan Nilai yang Akan Dipangkat: ");
+            int nilai = sc11.nextInt();
+            System.out.print("Masukkan Nilai Pangkat : ");
+            int nilaiPgkt = sc11.nextInt();
+            pangkatArr[i] = new Pangkat(nilai, nilaiPgkt);
+        }
+
+        System.out.println();
+        System.out.println("==Hasil - Pangkat Brute Force==");
+        for (int i = 0; i < pangkatArr.length; i++) {
+            System.out.println("Hasil Dari : " + pangkatArr[i].nilai + " Pangkat : " + pangkatArr[i].pangkat + " Adalah : ");
+            System.out.println(pangkatArr[i].pangkatBF(pangkatArr[i].nilai, pangkatArr[i].pangkat));
+        }
+        System.out.println();
+        System.out.println("==Hasil - Pangkat Divide Conquer==");
+        for (int i = 0; i < pangkatArr.length; i++) {
+            System.out.println("Hasil Dari : " + pangkatArr[i].nilai + " Pangkat : " + pangkatArr[i].pangkat + " Adalah : ");
+            System.out.println(pangkatArr[i].pangkatDC(pangkatArr[i].nilai, pangkatArr[i].pangkat));
+        }
+    }
+}
